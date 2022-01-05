5d8d74132dff9387a7d82f001b68c294d4ba0b4c
Moeketsi17 <89091903+Moeketsi17@users.noreply.github.com>
2022-01-05 16:30:22 +0200

    Add files via upload

---
+++ portfolio_/app.js
@@ -0,0 +1,39 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const links = document.querySelectorAll('.nav-links a')
+
+
+
+burger.addEventListener('click', () => {
+    nav.classList.toggle('active');
+
+    links.forEach((link, index) => {
+        if(link.style.animation){
+            link.style.animation = ''
+        } else {
+            link.style.animation = `navLinkFade 0.6s ease forwards ${index / 4 + 0.3}s `
+        }
+        console.log(index)
+    })
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ portfolio_/gsap.js
@@ -0,0 +1,21 @@
+// let tl1 = gsap.timeline ({
+//     scrollTrigger: {
+//         trigger: ".hero",
+//         start: "0%",
+//         end: "100%",
+//         scrub: 1,
+//         pin: true,
+//         pinSpacing: false,
+//     }
+// });
+
+// let tl2 = gsap.timeline ({
+//     scrollTrigger: {
+//         trigger: ".project-section",
+//         start: "%",
+//         end: "100%",
+//         scrub: 1,
+//         pin: true,
+//         pinSpacing: false,
+//     }
+// });
